a29ed1d6a03cb9ad040eb75e9877bf32bf8e2d0e
hsatig-mail <hsatig@mail.ru>
2018-03-20 19:33:12 +0500

    Module 3 completed

--- CityInfo/CityInfo.API/Models/CityDto.cs
+++ CityInfo/CityInfo.API/Models/CityDto.cs
@@ -11,6 +11,13 @@ namespace CityInfo.API.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest { get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } 
+            = new List<PointOfInterestDto>();
     }
 }
